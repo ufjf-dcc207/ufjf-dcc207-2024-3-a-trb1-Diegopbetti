cad326fa60c007e560f95c8d1687ba13b7458827
Diego Pereira Betti <diegopbetti.gmail.com>
2024-12-12 20:01:54 -0300

    Cria estrutura da ficha de exercicio

---
+++ src/components/Ficha.tsx
@@ -0,0 +1,13 @@
+function Ficha() {
+
+    return (
+      <>
+        <div className="container">
+        <h1>Ficha D</h1>
+        <div className="sub-container"></div>
+        </div>
+      </>
+    )
+  }
+  
+  export default Ficha
